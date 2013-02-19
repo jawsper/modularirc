4d11a2aacd4925327ba2c9c3ffa98a83b9bb3398
Jasper Seidel <vaio@jawsper.nl>
2013-02-19 22:57:38 +0100

    Whitespace consistency

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,8 @@
 import pkgutil, os
 
 def getmodules():
-	return [name for _,name, _ in pkgutil.iter_modules([os.path.dirname(__file__)])]
+	return [ name for _,name, _ in pkgutil.iter_modules( [ os.path.dirname( __file__ ) ] ) ]
 
 def getmodule( module ):
-	mod = __import__('modules.{0}'.format(module), fromlist=[module])
+	mod = __import__( 'modules.{0}'.format( module ), fromlist = [ module ] )
 	return getattr( mod, module )
