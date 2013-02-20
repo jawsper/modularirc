99c1dae3e4abe49cce8724b9e12690d8acdb7a7b
Jasper Seidel <vaio@jawsper.nl>
2013-02-20 02:12:50 +0100

    Moved MyLovelyIRCBot to Bot. Removed do_command and dodo_command.

--- Bot.py
+++ Bot.py
@@ -1,25 +1,54 @@
 from __future__ import print_function
+import ConfigParser, sys, os, signal, subprocess
 from ircbot import SingleServerIRCBot
-from irclib import nm_to_n, nm_to_uh, irc_lower
-import subprocess
-import os
+from irclib import nm_to_n, nm_to_uh, is_channel
+import modules
 
-class MyLovelyIRCBot( SingleServerIRCBot ):
-	modules = []
-	shutup = False
+class Bot( SingleServerIRCBot ):
+	def __init__( self ):
+		self.config = ConfigParser.SafeConfigParser()
+		self.config.read( os.path.expanduser( "~/.ircbot" ) )
+
+		s = self.config.get( "main", "server" ).split( ":", 1 )
+		server = s[0]
+		if len(s) == 2:
+			try:
+				port = int( s[1] )
+			except ValueError:
+				print( "Error: Erroneous port." )
+				sys.exit(1)
+		else:
+			port = 6667
 	
-	def __init__( self, channel, nickname, server, port=6667, password=None ):
+		password = self.config.get( "main", "password" )
+		
+		channel = self.config.get( 'main', 'channel' )
+		nickname = self.config.get( 'main', 'nickname' )
+		
 		if password != None:
 			SingleServerIRCBot.__init__( self, [( server, port, password )], nickname, nickname )
 		else:
 			SingleServerIRCBot.__init__( self, [( server, port )], nickname, nickname )
 		self.channel = channel
+		self.modules = []
+		self.admin = self.config.get( 'main', 'admin' )
+		for module in modules.getmodules():
+			self.add_module( module )
+		#self.bot = MyLovelyIRCBot( config.get( "main", "channel" ), config.get( "main", "nickname" ), server, port, password )
+		#self.bot.set_admin( config.get( "main", "admin" ) )
+		#for module in modules.getmodules():
+	#		self.bot.add_module( modules.getmodule( module )( config.items( module ) ) )
+		signal.signal( signal.SIGINT, self.sigint_handler )
 	
 	def add_module( self, module ):
-		self.modules.append( module )
+		self.modules.append( modules.getmodule( module )( self.config.items( module ) ) )
 	
-	def set_admin( self, admin ):
-		self.admin = admin
+	def sigint_handler( self, signal, frame ):
+		print( 'Ctrl+C pressed, shutting down!' )
+		self.die()
+		sys.exit(0)
+		
+	# from old bot
 
 	def on_nicknameinuse( self, c, e ):
 		print( "on_nicknameinuse" )
@@ -29,71 +58,29 @@ class MyLovelyIRCBot( SingleServerIRCBot ):
 		print( "on_welcome" )
 		c.join( self.channel )
 	
-	def on_join( self, c, e ):
-		print( "on_join {0}, {1}".format( e.target(), e.source() ) )
-		#if e.target() != "#" and nm_to_n( e.source() ) == c.get_nickname():
-		#	c.notice( e.target(), "I am alive! Muhahaha!" )
+#	def on_join( self, c, e ):
+#		print( "on_join {0}, {1}".format( e.target(), e.source() ) )
 		
-	def on_disconnect( self, c, e ):
-		print( "on_disconnect" )
-		
+#	def on_disconnect( self, c, e ):
+#		print( "on_disconnect" )
 
-	def on_privmsg( self, c, e ):
-		print( "on_privmsg" )
-		self.do_command( e, e.arguments()[0], nm_to_uh( e.source() ) == self.admin )
-
-	def on_pubmsg( self, c, e ):
-		print( "on_pubmsg" )
-		if e.arguments()[0][0] == '!':
-			self.do_command( e, e.arguments()[0][1:], nm_to_uh( e.source() ) == self.admin, False )
-		a = e.arguments()[0].split(":", 1)
-		if len(a) > 1 and irc_lower( a[0] ) == irc_lower( self.connection.get_nickname() ):
-			self.do_command( e, a[1].strip(), nm_to_uh( e.source() ) == self.admin )
+	def __process_command( self, c, e ):
+		message = e.arguments()[0]
+		if message[0] != '!':
 			return
-		return
-
-#	def on_dccmsg( self, c, e ):
-#		print( "on_dccmsg" )
-#		c.privmsg("You said: " + e.arguments()[0])
-#
-#	def on_dccchat( self, c, e ):
-#		print( "on_dccchat" )
-#		if len( e.arguments() ) != 2:
-#			return
-#		args = e.arguments()[1].split()
-#		if len( args ) == 4:
-#			try:
-#				address = ip_numstr_to_quad( args[2] )
-#				port = int( args[3] )
-#			except ValueError:
-#				return
-#			self.dcc_connect( address, port )
-
-	def do_command( self, e, args, admin = False, display_error = True ):
-#		print e.source()
-#		print e.target()
-#		print e.arguments()
-		args = args.split()
+		args = message[1:].split()
 		cmd = args.pop(0).strip()
-		self.dodo_command( e.source(), e.target(), cmd, args, admin, display_error )
-		
-	def dodo_command( self, source, target, cmd, args, admin = False, display_error = True ):
-		nick = nm_to_n( source )
-		c = self.connection
-		print( "do_command (src: {0}; tgt: {1}; cmd: {2}; args: {3})".format( source, target, cmd, args ) )
+		admin = nm_to_uh( e.source() ) == self.admin
+		nick = nm_to_n( e.source() )
+		target = e.target()
+		if not is_channel( target ):
+			target = nick
+		print( "__process_command (src: {0}; tgt: {1}; cmd: {2}; args: {3}; admin: {4})".format( nick, target, cmd, args, admin ) )
 		if admin:
-			if not self.shutup:
-			#	c.notice( nick, "Hiya boss!" )
-				self.shutup = True
-			
 			if cmd == "die":
 				c.notice( nick, "Goodbye cruel world!" )
 				self.die()
 				return
-			#elif cmd == "shutup":
-			#	c.notice( nick, "Sorry to bother you boss! :(" )
-			#	self.shutup = True
-			#	return
 			elif cmd == 'say':
 				t = args[0]
 				msg = args[1:]
@@ -160,12 +147,6 @@ class MyLovelyIRCBot( SingleServerIRCBot ):
 				r = subprocess.check_output( [ os.path.expanduser( '~/bin/ut-server' ), args[0] ] )
 				c.notice( nick, "Result: %s" % r )
 				return
-			#elif cmd == "dcc":
-			#	dcc = self.dcc_listen()
-			#	c.ctcp( "DCC", nick, "CHAT chat %s %d" % ( ip_quad_to_numstr( dcc.localaddress ), dcc.localport ) )
-			#else:
-			#	c.notice( nick, "Not understood: " + cmd )
-		#else:
 		for module in self.modules:
 			if module.handle_cmd( cmd ):
 #				try:
@@ -173,10 +154,19 @@ class MyLovelyIRCBot( SingleServerIRCBot ):
 					c.notice( target, line )
 #				except:
 #					c.notice( target, "A module borked..." )
-				break
+				return
 		if cmd == "potato":
 			c.notice( target, "I do quite enjoy potatoes" )
 		elif cmd == "open" and " ".join( args ) == "the pod bay doors":
-			c.notice( target, "I'm sorry, %s. I'm afraid I can't do that." % nm_to_n( source ) )
-		elif display_error:
-			c.notice( target, "Not understood: " + cmd )
+			c.notice( target, "I'm sorry, {0}. I'm afraid I can't do that.".format( nick ) )
+		#elif display_error:
+		#	c.notice( target, "Not understood: " + cmd )
+
+	def on_privmsg( self, c, e ):
+		print( "on_privmsg" )
+		self.__process_command( c, e )
+		#self.do_command( e, e.arguments()[0], nm_to_uh( e.source() ) == self.admin )
+
+	def on_pubmsg( self, c, e ):
+		print( "on_pubmsg" )
+		self.__process_command( c, e )
--- __main__.py
+++ __main__.py
@@ -1,49 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-
-import os, signal, ConfigParser, sys
-from MyLovelyIRCBot import MyLovelyIRCBot
-
-#modules
-import modules
-
-#import irclib
-#irclib.DEBUG = 1
-
-class Bot:
-	def __init__( self ):
-		config = ConfigParser.SafeConfigParser()
-		config.read( os.path.expanduser( "~/.ircbot" ) )
-
-		s = config.get( "main", "server" ).split( ":", 1 )
-		server = s[0]
-		if len(s) == 2:
-			try:
-				port = int( s[1] )
-			except ValueError:
-				print( "Error: Erroneous port." )
-				sys.exit(1)
-		else:
-			port = 6667
-	
-		password = config.get( "main", "password" )
-		self.bot = MyLovelyIRCBot( config.get( "main", "channel" ), config.get( "main", "nickname" ), server, port, password )
-		self.bot.set_admin( config.get( "main", "admin" ) )
-		for module in modules.getmodules():
-			self.bot.add_module( modules.getmodule( module )( config.items( module ) ) )
-		signal.signal( signal.SIGINT, self.sigint_handler )
-
-	def start( self ):
-		print( "Starting botje" )
-		self.bot.start()
-	def stop( self ):
-		print( 'Shutting down botje' )
-		self.bot.die()
-	def sigint_handler( self, signal, frame ):
-		print( 'Ctrl+C pressed, shutting down!' )
-		self.stop()
-		sys.exit(0)
+from Bot import Bot
 	
 if __name__ == '__main__':
 	print( "Welcome to botje" )
