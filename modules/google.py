64151287024f5f6606a452e946e8593a70128a9c
Jasper Seidel <tyrion@jawsper.nl>
2013-02-28 22:01:12 +0100

    Added google_clear_cache to google

--- modules/google.py
+++ modules/google.py
@@ -20,36 +20,39 @@ class google( _module ):
 				self.google_cache = {}
 				
 	def can_handle( self, cmd, admin ):
-		return self.api_key and self.cx and cmd == 'google'
+		return self.api_key and self.cx and cmd == 'google' or ( admin and cmd == 'google_clear_cache' )
 		
 	def handle( self, bot, cmd, args, source, target, admin ):
-		query = ' '.join( args )
-	
-		if not query in self.google_cache:
-			conn = httplib.HTTPSConnection( 'www.googleapis.com' )
-			conn.request(
-				'GET',
-				'/customsearch/v1?' + urllib.urlencode({
-					'cx': self.cx,
-					'key': self.api_key,
-					'q': query
-				})
-			)
-			response = conn.getresponse()
+		if admin and cmd == 'google_clear_cache':
+			self.google_cache = {}
+		if cmd == 'google':
+			query = ' '.join( args )
 		
-			self.google_cache[ query ] = json.load( response )
-			with open( self.google_cache_file, 'wb' ) as f:
-				pickle.dump( self.google_cache, f )
-		
-		results = self.google_cache[ query ]
-		if 'items' in results and results['items'] and results['items'][0]:
-			return [
-				"{0}: {1}".format(
-					results['items'][0]['title'].encode('ascii', 'ignore'),
-					results['items'][0]['link']
-				),
-				re.sub( ' {2,}', ' ', results['items'][0]['snippet'].encode( 'ascii', 'ignore' ) ),
-				'Meer resultaten: http://www.google.nl/search?q={0}'.format( query )
-			]
-		else:
-			return [ "I'm afraid I can't find that." ]
+			if not query in self.google_cache:
+				conn = httplib.HTTPSConnection( 'www.googleapis.com' )
+				conn.request(
+					'GET',
+					'/customsearch/v1?' + urllib.urlencode({
+						'cx': self.cx,
+						'key': self.api_key,
+						'q': query
+					})
+				)
+				response = conn.getresponse()
+			
+				self.google_cache[ query ] = json.load( response )
+				with open( self.google_cache_file, 'wb' ) as f:
+					pickle.dump( self.google_cache, f )
+			
+			results = self.google_cache[ query ]
+			if 'items' in results and results['items'] and results['items'][0]:
+				return [
+					"{0}: {1}".format(
+						results['items'][0]['title'].encode('ascii', 'ignore'),
+						results['items'][0]['link']
+					),
+					re.sub( ' {2,}', ' ', results['items'][0]['snippet'].encode( 'ascii', 'ignore' ) ),
+					'Meer resultaten: http://www.google.nl/search?q={0}'.format( query )
+				]
+			else:
+				return [ "I'm afraid I can't find that." ]
