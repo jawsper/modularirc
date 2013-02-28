1b3576f5a36921f1e6bb58e7a0a347d6b51cdbb5
Jasper Seidel <tyrion@jawsper.nl>
2013-02-28 16:32:07 +0100

    Added "-nofork" option.

--- __main__.py
+++ __main__.py
@@ -6,14 +6,19 @@ from Bot import Bot
 	
 if __name__ == '__main__':
 	print( "Welcome to botje" )
-	try:
-		pid = os.fork()
-		if pid > 0:
-			print( 'Forked into PID {0}'.format( pid ) )
-			sys.exit(0)
-		sys.stdout = open( '/tmp/ircbot.log', 'w' )
-	except OSError as error:
-		print( 'Unable to fork. Error: {0} ({1})'.format( error.errno, error.strerror ) )
-		sys.exit(1)
+	fork = True
+	if len( sys.argv ) > 1:
+		if sys.argv[1] == '-nofork':
+			fork = False
+	if fork:
+		try:
+			pid = os.fork()
+			if pid > 0:
+				print( 'Forked into PID {0}'.format( pid ) )
+				sys.exit(0)
+			sys.stdout = open( '/tmp/ircbot.log', 'w' )
+		except OSError as error:
+			print( 'Unable to fork. Error: {0} ({1})'.format( error.errno, error.strerror ) )
+			sys.exit(1)
 	botje = Bot()
 	botje.start()
