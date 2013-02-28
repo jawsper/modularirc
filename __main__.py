61ddc43292999bcce000e4ec3d346758de93be67
Jasper Seidel <tyrion@jawsper.nl>
2013-02-28 12:24:45 +0100

    Forking into child process when running.

--- __main__.py
+++ __main__.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import os, sys
 from Bot import Bot
 	
 if __name__ == '__main__':
 	print( "Welcome to botje" )
+	try:
+		pid = os.fork()
+		if pid > 0:
+			print( 'Forked into PID {0}'.format( pid ) )
+			sys.exit(0)
+	except OSError as error:
+		print( 'Unable to fork. Error: {0} ({1})'.format( error.errno, error.strerror ) )
+		sys.exit(1)
 	botje = Bot()
 	botje.start()
