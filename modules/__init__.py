b57075c662775d97f4225576d67e6a54d73a6b82
Jasper Seidel <tyrion@jawsper.nl>
2013-02-28 15:32:09 +0100

    Added comments to modules/__init__. Modules starting with __ do not get loaded.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,23 @@
 import pkgutil, os, sys
 
 def getmodules():
-	return [ name for _,name, _ in pkgutil.iter_modules( [ os.path.dirname( __file__ ) ] ) ]
+	"""Returns all modules that are found in the current package.
+	Excludes modules starting with '__'"""
+	return [ name for _,name, _ in pkgutil.iter_modules( [ os.path.dirname( __file__ ) ] ) if name[0:2] != '__' ]
 
 def getmodule( module ):
+	"""Import module <module> and return the class.
+	This returns the class modules.<module>.<module>"""
 	mod = __import__( 'modules.{0}'.format( module ), fromlist = [ module ] )
 	return getattr( mod, module )
 
 def reload_module( module ):
+	"""Reload a module given the module name. 
+	This should be just the name, not the full module path.
+	
+	Arguments:
+	module: the name of the module
+	"""
 	try:
 		reload( getattr( sys.modules[ __name__ ], module ) )
 	except AttributeError:
