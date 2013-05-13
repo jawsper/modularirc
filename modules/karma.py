dff3e6af15aceec82303191bcb7989a83eceb4e4
Jasper Seidel <tyrion@jawsper.nl>
2013-05-13 18:19:57 +0200

    karma: show reply as message based on scoring, deny karma giving to anything containing spaces, show a * in front of karma list items

--- modules/karma.py
+++ modules/karma.py
@@ -7,18 +7,23 @@ class karma( _module ):
 	def on_privmsg( self, bot, source, target, message ):
 		if message[-2:] == '++' or message[-2:] == '--':
 			item = message[ :-2 ]
+			if ' ' in item: # no spaces
+				return
 			scoring = 1 if message[-2:] == '++' else -1
 			if not item in self.karma:
 				self.karma[ item ] = 0
 			self.karma[ item ] += scoring
-			bot.notice( source, '{0}: {1}'.format( item, self.karma[ item ] ) )
+			reply = '\x02{0}\x0F gave \x02{1}\x0F some sweet karma (now at {2})' if scoring > 0 else '\x02{0}\x0F dislikes \x02{1}\x0F, and removed karma (now at {2})'
+			bot.notice( target, reply.format( source, item, self.karma[ item ] ) )
 	def can_handle( self, cmd, admin ):
 		return cmd in ( 'karma', )
 	def handle( self, bot, cmd, args, source, target, admin ):
 		if cmd == 'help':
 			return [ '!karma: shows all karma' ]
 		elif cmd == 'karma':
+			if len( self.karma ) == 0:
+				return [ 'No karma given out yet. (Maybe you should!)' ]
 			return [
 				'Karma:'
-			] + map( lambda ( item, karma ): '{0}: {1}'.format( item, karma ), self.karma.iteritems() )
+			] + map( lambda ( item, karma ): ' * {0}: {1}'.format( item, karma ), self.karma.iteritems() )
 			
