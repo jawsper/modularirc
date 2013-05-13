bdcc177c8c551cde7cc65818fe70c95011fb93ad
Jasper Seidel <tyrion@jawsper.nl>
2013-05-13 19:12:12 +0200

    karma: added !karmawhy

--- modules/karma.py
+++ modules/karma.py
@@ -23,9 +23,18 @@ class karma( _module ):
 			item = message[ :-2 ]
 			scoring = 1 if message[-2:] == '++' else -1
 			self.karma.append( { 'item': item, 'mutation': scoring, 'source': source, 'comment': comment } )
-			reply = '\x02{0}\x0F gave \x02{1}\x0F some sweet karma' if scoring > 0 else '\x02{0}\x0F dislikes \x02{1}\x0F, and removed karma'
-			bot.notice( target, reply.format( source, item ) )
-		
+			reply = '\x02{0}\x0F gave "{1}" some sweet karma (now at {2})' if scoring > 0 else '\x02{0}\x0F dislikes "{1}", and removed karma (now at {2})'
+			bot.notice( target, reply.format( source, item, self.item_karma( item ) ) )
+	
+	def item_karma_list( self, item ):
+		return filter( lambda v: v['item'] == item, self.karma )
+	
+	def item_karma( self, item ):
+		karma = 0
+		for evt in filter( lambda v: v['item'] == item, self.karma ):
+			karma += evt['mutation']
+		return karma
+	
 	def total_karma( self ):
 		items = {}
 		for evt in self.karma:
@@ -36,16 +45,27 @@ class karma( _module ):
 		return items
 	
 	def can_handle( self, cmd, admin ):
-		return cmd in ( 'karma', )
+		return cmd in ( 'karma', 'karmawhy' )
 	
 	def handle( self, bot, cmd, args, source, target, admin ):
 		if cmd == 'help':
-			return [ '!karma: shows all karma' ]
+			return [ '!karma: shows all karma', '!karmawhy [<what>]: show last karma, optionally for item name' ]
 		elif cmd == 'karma':
 			if len( self.karma ) == 0:
 				return [ 'No karma given out yet. (Maybe you should!)' ]
 			return [
 				'Karma:'
 			] + map( lambda ( item, karma ): ' * {0}: {1}'.format( item, karma ), self.total_karma().iteritems() )
-			
+		elif cmd == 'karmawhy':
+			if len( self.karma ) == 0:
+				return
+			karma_format = '{source} gave "{item}" {mutation} because "{comment}"'
+			if len( args ) == 0:
+				return [ 'Last karma: ', karma_format.format( **self.karma[ -1 ] ) ]
+			elif len( args ) == 1:
+				item = args[0]
+				karma = self.item_karma_list( item )
+				if len( karma ) == 0:
+					return [ 'Karma has not been applied to "{0}"'.format( item ) ]
+				return [ 'Last karma for "{0}":'.format( item ), karma_format.format( **karma[ -1 ] ) ]
 			
