4c0fbb0ebbb4acb405eb7d963c5e594ccd75a2ee
Jasper Seidel <tyrion@jawsper.nl>
2013-05-13 18:59:01 +0200

    karma: change storage to list of mutations; require msg to start with !; allow comments; allow spaces; change display to use total_karma method

--- modules/karma.py
+++ modules/karma.py
@@ -3,20 +3,41 @@ from _module import _module
 class karma( _module ):
 	def __init__( self, config, bot ):
 		super( karma, self ).__init__( config, bot )
-		self.karma = {}
+		self.karma = []
+	
 	def on_privmsg( self, bot, source, target, message ):
+		
+		# require starting !
+		if message[0] != '!':
+			return
+		# strip it off
+		message = message[1:]
+		
+		# find comment
+		comment = None
+		if '#' in message:
+			comment = message[ message.find( '#' ) + 1 : ].strip()
+			message = message[ : message.find( '#' ) ].strip()
+		
 		if message[-2:] == '++' or message[-2:] == '--':
 			item = message[ :-2 ]
-			if ' ' in item: # no spaces
-				return
 			scoring = 1 if message[-2:] == '++' else -1
-			if not item in self.karma:
-				self.karma[ item ] = 0
-			self.karma[ item ] += scoring
-			reply = '\x02{0}\x0F gave \x02{1}\x0F some sweet karma (now at {2})' if scoring > 0 else '\x02{0}\x0F dislikes \x02{1}\x0F, and removed karma (now at {2})'
-			bot.notice( target, reply.format( source, item, self.karma[ item ] ) )
+			self.karma.append( { 'item': item, 'mutation': scoring, 'source': source, 'comment': comment } )
+			reply = '\x02{0}\x0F gave \x02{1}\x0F some sweet karma' if scoring > 0 else '\x02{0}\x0F dislikes \x02{1}\x0F, and removed karma'
+			bot.notice( target, reply.format( source, item ) )
+		
+	def total_karma( self ):
+		items = {}
+		for evt in self.karma:
+			item = evt['item']
+			if not item in items:
+				items[ item ] = 0
+			items[ item ] += evt['mutation']
+		return items
+	
 	def can_handle( self, cmd, admin ):
 		return cmd in ( 'karma', )
+	
 	def handle( self, bot, cmd, args, source, target, admin ):
 		if cmd == 'help':
 			return [ '!karma: shows all karma' ]
@@ -25,5 +46,6 @@ class karma( _module ):
 				return [ 'No karma given out yet. (Maybe you should!)' ]
 			return [
 				'Karma:'
-			] + map( lambda ( item, karma ): ' * {0}: {1}'.format( item, karma ), self.karma.iteritems() )
+			] + map( lambda ( item, karma ): ' * {0}: {1}'.format( item, karma ), self.total_karma().iteritems() )
+			
 			
