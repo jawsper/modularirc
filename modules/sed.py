63cb6085baee9ce5d5bd0e15b9bf43604ab184b4
Jasper Seidel <tyrion@jawsper.nl>
2013-05-14 16:05:11 +0200

    sed buffer to 100, append to front instead of back

--- modules/sed.py
+++ modules/sed.py
@@ -5,14 +5,18 @@ class sed( _module ):
 	def __init__( self, config, bot ):
 		super( sed, self ).__init__( config, bot )
 		self.buffer = []
+		self.buffer_max = 100
 	
 	def on_privmsg( self, bot, source, target, message ):
 		m = re.match( 's/(?P<from>[^/]+)/(?P<to>.*)$', message )
 		if m:
 			for item in self.buffer:
 				if re.search( m.group('from'), item['message'] ):
-					bot.notice( target, '<{0}> {1}'.format( item['source'], re.sub( m.group('from'), m.group('to'), item['message'] ) ) )
+					new_message = re.sub( m.group('from'), m.group('to'), item['message'] )
+					bot.privmsg( target, '<{0}> {1}'.format( item['source'], new_message ) )
+					self.buffer.insert( 0, { 'source': item['source'], 'target': item['target'], 'message': new_message } )
+					break
 		else:
-			self.buffer.append( { 'source': source, 'target': target, 'message': message } )
-			if len( self.buffer ) > 10:
-				self.buffer = self.buffer[ 1: ]
+			self.buffer.insert( 0, { 'source': source, 'target': target, 'message': message } )
+			if len( self.buffer ) > self.buffer_max:
+				self.buffer.pop()
