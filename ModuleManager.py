fccb9d5f9869bc9663d77332d4d89286f3168423
Jasper Seidel <sammy@jawsper.nl>
2013-05-20 18:04:30 +0200

    Created class ModuleManager

---
+++ ModuleManager.py
@@ -0,0 +1,16 @@
+class ModuleManager:
+	def __init__( self, bot ):
+		self.bot = bot
+		self.modules = {}
+		self.loaded_modules = []
+	
+	def add_module( self, module_name ):
+		pass
+	def remove_module( self, module_name ):
+		pass
+	def enable_module( self, module_name ):
+		pass
+	def disable_module( self, module_name ):
+		pass
+	def reload_module( self, module_name ):
+		pass
