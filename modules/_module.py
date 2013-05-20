4b5170dcab87aa4758141cceca6614e6f3e7d4cc
Jasper Seidel <tyrion@jawsper.nl>
2013-05-20 23:28:54 +0200

    _module: print when loading, added get/set_config

--- modules/_module.py
+++ modules/_module.py
@@ -1,14 +1,22 @@
 class _module(object):
 	def __init__( self, manager ):
 		self.mgr = manager
+		print( 'Loading module {0}'.format( self.__class__.__name__ ) )
 	def stop(self):
 		pass
 	def can_handle( self, cmd, admin ):
 		return False
 	def handle( self, cmd, args, source, target, admin ):
 		pass
+		
+	# methods that directly call the mgr
 
 	def notice( self, target, message ):
 		self.mgr.notice( target, message )
 	def privmsg( self, target, message ):
 		self.mgr.privmsg( target, message )
+		
+	def get_config( self, key ):
+		return self.mgr.get_config( self.__class__.__name__, key )
+	def set_config( self, key, value ):
+		self.mgr.set_config( self.__class__.__name__, key, value )
