6290579f768d7e6d396a606bfc98bd28362cfcec
Jasper Seidel <sammy@jawsper.nl>
2013-05-20 19:32:23 +0200

    ModuleManager: on __init__ find all modules; added help texts

--- ModuleManager.py
+++ ModuleManager.py
@@ -5,19 +5,28 @@ class ModuleManager( object ):
 		self.bot = bot
 		self.modules = {}
 		self.loaded_modules = {}
+		for module_name in modules.get_modules():
+			self.add_module( module_name )
+
+	def reload_modules( self ):
+		pass
 
 	def get_modules( self ):
+		"""Get all found modules"""
 		return self.modules.iterkeys()
 
 	def get_loaded_modules( self ):
+		"""Get all loaded modules"""
 		return self.loaded_modules
 
 	def get_available_modules( self ):
+		"""Get all available modules: modules that are found but not loaded"""
 		modules = filter( lambda key: key not in self.loaded_modules, self.modules.keys() )
 		modules.sort()
 		return modules
 	
 	def add_module( self, module_name ):
+		"""Load a module"""
 		if module_name in self.modules:
 			return 'Module already available'
 		try:
@@ -30,6 +39,7 @@ class ModuleManager( object ):
 		return 'Module not available'
 
 	def remove_module( self, module_name ):
+		"""Unload a module"""
 		if not module_name in self.modules:
 			return 'Module not available'
 		if module_name in self.loaded_modules:
@@ -38,6 +48,7 @@ class ModuleManager( object ):
 		return 'Module removed'
 			
 	def enable_module( self, module_name ):
+		"""Enable a module"""
 		if not module_name in self.modules:
 			return 'Module not available'
 		if module_name in self.loaded_modules:
@@ -49,6 +60,7 @@ class ModuleManager( object ):
 		return 'Module enabled'
 
 	def disable_module( self, module_name ):
+		"""Disable a module"""
 		if not module_name in self.loaded_modules:
 			return 'Module not enabled'
 		try:
@@ -59,10 +71,17 @@ class ModuleManager( object ):
 		return 'Module disabled'
 
 	def reload_module( self, module_name ):
+		"""Reload a module"""
 		if not module_name in self.modules:
 			return 'Module not available'
 		if module_name in self.loaded_modules:
 			self.disable_module( module_name )
 		self.enable_module( module_name )
 		return 'Module reloaded'
+
+	def get_config( self, group, key, value ):
+		pass
+
+	def set_config( self, group, key ):
+		pass
 
