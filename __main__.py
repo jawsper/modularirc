c18389855ce17ffdbf87afffca0dd006e90395c2
Jasper Seidel <tyrion@jawsper.nl>
2013-06-11 14:38:41 +0200

    Added changed from master

--- __main__.py
+++ __main__.py
@@ -5,23 +5,26 @@ import Bot
 import logging
 
 from imp import reload
+
+logging_level = logging.DEBUG
+logging_format = '[%(asctime)s] %(levelname)s: %(message)s'
 	
 if __name__ == '__main__':
-	logging.basicConfig( 
-		#filename = 'ircbot.log', 
-		level = logging.DEBUG, format = '[%(asctime)s] %(levelname)s: %(message)s' )
-	logging.info( "Welcome to botje" )
 	fork = True
 	if len( sys.argv ) > 1:
 		if sys.argv[1] == '-nofork':
 			fork = False
+	if fork:
+		logging.basicConfig( filename = 'ircbot.log', level = logging_level, format = logging_format )
+	else:
+		logging.basicConfig( level = logging_level, format = logging_format )
+	logging.info( "Welcome to botje" )
 	if fork:
 		try:
 			pid = os.fork()
 			if pid > 0:
 				logging.info( 'Forked into PID {0}'.format( pid ) )
 				sys.exit(0)
-			sys.stdout = open( '/tmp/ircbot.log', 'w' )
 		except OSError as error:
 			logging.error( 'Unable to fork. Error: {0} ({1})'.format( error.errno, error.strerror ) )
 			sys.exit(1)
@@ -32,8 +35,9 @@ if __name__ == '__main__':
 		except Bot.BotReloadException:
 			logging.info( 'Force reloading Bot class' )
 			botje = None
-			reload(Bot)
+			reload( Bot )
 			botje = Bot.Bot()
+			continue
 		logging.info( 'Botje died, restarting in 5...' )
 		import time
 		time.sleep( 5 )
