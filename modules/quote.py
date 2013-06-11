d250c35ceb77c7f5e64d5227ecea2216747d9bf5
Jasper Seidel <sammy@jawsper.nl>
2013-06-11 23:30:37 +0200

    Fixed quote module

--- modules/quote.py
+++ modules/quote.py
@@ -1,16 +1,20 @@
 from ._module import _module
+import logging
+import random
 
-class quote( _module ):		
+class quote( _module ):
 	def cmd_quote( self, args, source, target, admin ):
 		"""!quote: to get a random quote"""
 		return [ self.__random_quote() ]
-	
+
 	def __random_quote( self ):
 		"""Read a quote from a text file"""
 		try:
 			with open( self.get_config( 'quote_file' ) ) as fd:
 				return random.choice( fd.readlines() )
-		except IOError:
-			return 'Error: quote file not found'
+		except IOError as e:
+			logging.exception( 'Quote IOError' )
+			return 'Error: quote file not found: {}'.format( e )
 		except:
+			logging.exception( 'Quote Exception' )
 			return 'Error: no quote file defined'
