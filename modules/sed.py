c0a74c8cb24965ef9eaeb898a5b366c0f503fb25
Jasper Seidel <lenny@jawsper.nl>
2014-10-16 23:33:36 +0200

    Modified sed module to only look at messages from same target.

--- modules/sed.py
+++ modules/sed.py
@@ -5,21 +5,24 @@ class sed( _module ):
 	"""sed: implements sed's s// partially."""
 	def __init__( self, mgr ):
 		super( sed, self ).__init__( mgr )
-		self.buffer = []
+		self.buffer = {}
 		self.buffer_max = 100
 	
 	def on_privmsg( self, source, target, message ):
 		m = re.match( 's/(?P<from>[^/]+)/(?P<to>.*)/$', message )
+		if not target in self.buffer:
+			self.buffer[target] = []
+		buff = self.buffer[target]
 		if m:
-			for item in self.buffer:
+			for item in buff:
 				if item['message'] + '/' == message: # fix for me typing the damn command wrong
 					continue
 				if re.search( m.group('from'), item['message'] ):
 					new_message = re.sub( m.group('from'), m.group('to'), item['message'] )
 					self.privmsg( target, '<{0}> {1}'.format( item['source'], new_message ) )
-					self.buffer.insert( 0, { 'source': item['source'], 'target': item['target'], 'message': new_message } )
+					buff.insert( 0, { 'source': item['source'], 'target': item['target'], 'message': new_message } )
 					break
 		else:
-			self.buffer.insert( 0, { 'source': source, 'target': target, 'message': message } )
-			if len( self.buffer ) > self.buffer_max:
-				self.buffer.pop()
+			buff.insert( 0, { 'source': source, 'target': target, 'message': message } )
+			if len( buff ) > self.buffer_max:
+				buff.pop()
