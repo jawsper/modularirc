253de2e7b7083cc2c8c1f5d3990ed54b52158936
Jasper Seidel <lenny@jawsper.nl>
2014-12-11 17:02:05 +0100

    Made reminder store date and target case-insensitive.

--- modules/reminder.py
+++ modules/reminder.py
@@ -2,6 +2,9 @@ from ._module import _module
 import logging
 import json
 
+from datetime import datetime
+from dateutil.tz import tzlocal
+
 class reminder( _module ):
     def __init__(self, mgr):
         _module.__init__(self, mgr)
@@ -15,21 +18,25 @@ class reminder( _module ):
     def on_join(self, c, e):
         name = e.source.nick
         if name is not c.get_nickname():
+            name = name.lower()
             logging.debug('%s joined %s', name, e.target)
             if name in self.reminders:
-                for reminder_source, reminder_message in self.reminders[name].items():
-                    self.notice(e.target, 'Welcome {}, <{}> wanted you to know this: {}'.format(name, reminder_source, reminder_message))
+                for sender, reminder in self.reminders[name].items():
+                    self.notice(e.target, 'Welcome {}, <{}> said this at {date} for you: {message}'.format(e.source.nick, sender, **reminder))
                 del self.reminders[name]
 
     def cmd_reminder(self, args, source, target, admin):
         """!reminder <name> [<message>]: send <name> a message when they join, if message is empty then reminder will be cleared."""
         if len(args) < 1:
             return [self.cmd_reminder.__doc__]
-        name = args[0]
+        name = args[0].lower()
         message = ' '.join(args[1:])
         for channel_name, channel in self.mgr.bot.channels.items():
             if channel.has_user(name):
                 return ['User {} is already present'.format(name)]
+
+        date = datetime.now(tzlocal()).strftime('%Y-%m-%d %H:%M:%S%z')
+        reminder = {'date': date, 'message': message}
 
         if name in self.reminders:
             if source in self.reminders[name]:
@@ -37,18 +44,18 @@ class reminder( _module ):
                     del self.reminders[name][source]
                     return ['Reminder cleared']
                 else:
-                    self.reminders[name][source] = message
+                    self.reminders[name][source] = reminder
                     return ['New reminder set']
             else:
                 if len(message) == 0:
                     return ['No reminder to be cleared']
                 else:
-                    self.reminders[name][source] = message
+                    self.reminders[name][source] = reminder
                     return ['Reminder set']
         else:
             if len(message) == 0:
                 return ['No reminder to be cleared']
             else:
                 self.reminders[name] = {}
-                self.reminders[name][source] = message
+                self.reminders[name][source] = reminder
                 return ['Reminder set']
