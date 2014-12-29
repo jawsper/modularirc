d569b3df6b39cf5d5d4e893a98f9974526d50c37
Jasper Seidel <lenny@jawsper.nl>
2014-12-29 19:00:36 +0100

    >:)

--- modules/koeien.py
+++ modules/koeien.py
@@ -5,5 +5,5 @@ KOEIEN_TEKST = 'GEEN KOEIEN'
 
 class koeien(_module):
 	def on_privmsg(self, source, target, message):
-		if message in KOEIEN:
+		if message.lower() in KOEIEN:
 			self.mgr.bot.connection.kick(target, source, KOEIEN_TEKST)
