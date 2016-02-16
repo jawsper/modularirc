c1112ed9fd70b093ec1e7c688ed4202224b7add4
Jasper Seidel <lenny@jawsper.nl>
2016-02-16 09:37:37 +0100

    Make koeien detect more things.

--- modules/koeien.py
+++ modules/koeien.py
@@ -1,9 +1,13 @@
-from ._module import _module
+from modules import Module
+
+import re
 
 KOEIEN = ('boe', 'moe')
 KOEIEN_TEKST = 'GEEN KOEIEN'
 
-class koeien(_module):
+KOEIEN_REGEX = r'^[mb]o+e+[.?!]*$'
+
+class koeien(Module):
 	def on_privmsg(self, source, target, message):
-		if message.lower() in KOEIEN:
+		if message.lower() in KOEIEN or re.search(KOEIEN_REGEX, message.lower()):
 			self.mgr.bot.connection.kick(target, source, KOEIEN_TEKST)
