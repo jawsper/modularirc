92af394e21ea9f81d2a9953c79c50e160eb0e680
Jasper Seidel <lenny@jawsper.nl>
2016-02-17 20:38:20 +0100

    Tabs to spaces.

--- modules/google.py
+++ modules/google.py
@@ -4,75 +4,75 @@ import http.client, urllib.request, urllib.parse, urllib.error
 from modules import Module
 
 class google(Module):
-	"""Bot module to search on google"""
-	google_cache_file = os.path.expanduser( '~/.google_cache' )
+    """Bot module to search on google"""
+    google_cache_file = os.path.expanduser( '~/.google_cache' )
 
-	def start(self):
-		self.api_key = self.cx = None
+    def start(self):
+        self.api_key = self.cx = None
 
-		try:
-			self.api_key = self.get_config( 'api_key' )
-			self.cx = self.get_config( 'cx' )
-		except:
-			raise Exception
+        try:
+            self.api_key = self.get_config( 'api_key' )
+            self.cx = self.get_config( 'cx' )
+        except:
+            raise Exception
 
-		self.load_cache()
+        self.load_cache()
 
-	def load_cache(self):
-		if not os.path.exists( self.google_cache_file ):
-			with open( self.google_cache_file, 'w' ):
-				pass
-		with open( self.google_cache_file, 'rb' ) as f:
-			try:
-				self.google_cache = pickle.load(f)
-				if not isinstance(self.google_cache, dict):
-					self.google_cache = {}
-			except EOFError:
-				self.google_cache = {}
+    def load_cache(self):
+        if not os.path.exists( self.google_cache_file ):
+            with open( self.google_cache_file, 'w' ):
+                pass
+        with open( self.google_cache_file, 'rb' ) as f:
+            try:
+                self.google_cache = pickle.load(f)
+                if not isinstance(self.google_cache, dict):
+                    self.google_cache = {}
+            except EOFError:
+                self.google_cache = {}
 
-	def save_cache(self):
-		with open(self.google_cache_file, 'wb') as f:
-			pickle.dump(self.google_cache, f)
+    def save_cache(self):
+        with open(self.google_cache_file, 'wb') as f:
+            pickle.dump(self.google_cache, f)
 
-	def admin_cmd_google_clear_cache(self, admin, **kwargs):
-		"""!google_cache_clear: Clear the google cache"""
-		if not admin:
-			return
-		self.google_cache = {}
-	
-	def cmd_google(self, raw_args, **kwargs):
-		"""!google <query>: Search on google"""
-		if not self.api_key and self.cx:
-			return
-			
-		query = raw_args
+    def admin_cmd_google_clear_cache(self, admin, **kwargs):
+        """!google_cache_clear: Clear the google cache"""
+        if not admin:
+            return
+        self.google_cache = {}
+    
+    def cmd_google(self, raw_args, **kwargs):
+        """!google <query>: Search on google"""
+        if not self.api_key and self.cx:
+            return
+            
+        query = raw_args
 
-		if not query in self.google_cache:
-			conn = http.client.HTTPSConnection( 'www.googleapis.com' )
-			conn.request(
-				'GET',
-				'/customsearch/v1?' + urllib.parse.urlencode({
-					'cx': self.cx,
-					'key': self.api_key,
-					'q': query
-				})
-			)
-			response = conn.getresponse().read().decode('utf-8')
+        if not query in self.google_cache:
+            conn = http.client.HTTPSConnection( 'www.googleapis.com' )
+            conn.request(
+                'GET',
+                '/customsearch/v1?' + urllib.parse.urlencode({
+                    'cx': self.cx,
+                    'key': self.api_key,
+                    'q': query
+                })
+            )
+            response = conn.getresponse().read().decode('utf-8')
 
-			self.google_cache[ query ] = json.loads( response )
-			self.save_cache()
+            self.google_cache[ query ] = json.loads( response )
+            self.save_cache()
 
-		results = self.google_cache[ query ]
-		if 'items' in results and results['items'] and results['items'][0]:
-			snippet = re.sub('[\r\n]', ' ', results['items'][0]['snippet'])
-			snippet = snippet.replace('  ', ' ')
-			return [
-				"{0}: {1}".format(
-					results['items'][0]['title'],
-					results['items'][0]['link']
-				),
-				re.sub( ' {2,}', ' ', snippet),
-				'Meer resultaten: http://www.google.nl/search?q={0}'.format(query.replace(' ', '+'))
-			]
-		else:
-			return [ "I'm afraid I can't find that." ]
+        results = self.google_cache[ query ]
+        if 'items' in results and results['items'] and results['items'][0]:
+            snippet = re.sub('[\r\n]', ' ', results['items'][0]['snippet'])
+            snippet = snippet.replace('  ', ' ')
+            return [
+                "{0}: {1}".format(
+                    results['items'][0]['title'],
+                    results['items'][0]['link']
+                ),
+                re.sub( ' {2,}', ' ', snippet),
+                'Meer resultaten: http://www.google.nl/search?q={0}'.format(query.replace(' ', '+'))
+            ]
+        else:
+            return [ "I'm afraid I can't find that." ]
--- modules/ns.py
+++ modules/ns.py
@@ -10,261 +10,261 @@ from modules import Module
 import logging
 
 class NsApiException(Exception):
-	pass
+    pass
 
 class ns(Module):
-	"""ns: Bot module to use the NS (Nederlandse Spoorwegen) API"""
-	def start(self):
-		self.username = self.password = None
-		
-		try:
-			self.username = self.get_config( 'username' )
-			self.password = self.get_config( 'password' )
-		except:
-			raise Exception
-		
-		try:
-			self.stations = self.__station_list()
-		except Exception as e:
-			logging.warning( 'Loading stations failed: %s', e )
-			raise e
-		
-	def cmd_ns(self, arglist, **kwargs):
-		"""!ns <command>: search train connections (send !ns help for more details)"""
-		if len( arglist ) > 0 and arglist[0] != 'help':
-			try:
-				subcmd = arglist[0]
-				args = arglist[1:]
-				strarg = ' '.join( args )
-				if subcmd == 'plan':
-					return self.__plan_route( args )
-				elif subcmd == 'vtijden':
-					if len( strarg ) == 0:
-						strarg = 'enschede'
-					return self.__vertrektijden( strarg )
-				elif subcmd == 'storing':
-					return self.__storingen( strarg )
-			except:
-				traceback.print_exc()
-				return [ 'Bot error\'d :(' ]
-		return [
-			'Usage: !ns <command> <arguments>',
-			'Commands:',
-			'help (no arguments): this help text',
-			'plan (<fromStation> [<viaStation>] <toStation>): plan route from a to b (optionally via c)',
-			'vtijden [<station>]: departure times (default Enschede)',
-			'storing [<station>]: storingen (station optioneel)'
-		]
+    """ns: Bot module to use the NS (Nederlandse Spoorwegen) API"""
+    def start(self):
+        self.username = self.password = None
+        
+        try:
+            self.username = self.get_config( 'username' )
+            self.password = self.get_config( 'password' )
+        except:
+            raise Exception
+        
+        try:
+            self.stations = self.__station_list()
+        except Exception as e:
+            logging.warning( 'Loading stations failed: %s', e )
+            raise e
+        
+    def cmd_ns(self, arglist, **kwargs):
+        """!ns <command>: search train connections (send !ns help for more details)"""
+        if len( arglist ) > 0 and arglist[0] != 'help':
+            try:
+                subcmd = arglist[0]
+                args = arglist[1:]
+                strarg = ' '.join( args )
+                if subcmd == 'plan':
+                    return self.__plan_route( args )
+                elif subcmd == 'vtijden':
+                    if len( strarg ) == 0:
+                        strarg = 'enschede'
+                    return self.__vertrektijden( strarg )
+                elif subcmd == 'storing':
+                    return self.__storingen( strarg )
+            except:
+                traceback.print_exc()
+                return [ 'Bot error\'d :(' ]
+        return [
+            'Usage: !ns <command> <arguments>',
+            'Commands:',
+            'help (no arguments): this help text',
+            'plan (<fromStation> [<viaStation>] <toStation>): plan route from a to b (optionally via c)',
+            'vtijden [<station>]: departure times (default Enschede)',
+            'storing [<station>]: storingen (station optioneel)'
+        ]
 
-	def __apiquery( self, api_method, args = None ):
-		conn = http.client.HTTPConnection( 'webservices.ns.nl' )
-		conn.request(
-			'GET',
-			'/{0}{1}'.format( api_method, '?' + urllib.parse.urlencode( args ) if args and len( args ) > 0 else '' ),
-			headers = { 'Authorization': 'Basic ' + base64.b64encode( '{0}:{1}'.format( self.username, self.password ).encode( 'utf-8' ) ).decode( 'utf-8' ) }
-		)
-		response = conn.getresponse()
-		data = response.read()
-		conn.close()
-		root = ET.fromstring( data )
-		if root.tag == 'error':
-			raise NsApiException( 'API error: {0}'.format( root[0].text ) )
-		return root
-	
-	def __station_list( self ):
-		try:
-			root = self.__apiquery( 'ns-api-stations-v2' )
-		except NsApiException as e:
-			return [ str( e ) ]
-		if root.tag == 'Stations':
-			list = {}
-			for station in root:
-				namen = []
-				code = station.find( 'Code' ).text
-				land = station.find( 'Land' ).text
-				for naam in station.find( 'Namen' ):
-					if not naam.text in namen:
-						namen.append( naam.text )
-				for naam in station.find( 'Synoniemen' ):
-					if not naam.text in namen:
-						namen.append( naam.text )
-				list[ code ] = {
-					'land': land,
-					'namen': namen
-				}
-			return list
-		raise Exception
-	
-	def __get_station_code( self, arg ):
-		for code in self.stations:
-			for name in self.stations[ code ]['namen']:
-				if name.lower() in arg:
-					return ( code, name.lower() )
-		return ( False, False )
-	
-	def __make_station_search_args( self, args ):
-		list = []
-		for i in range( 0, len( args ) + 1 ):
-			list.append( ' '.join( args[ :i ] ) )
-		return list
-	
-	def __plan_route( self, args ):
-		print( '__plan_route( {0} )'.format( args ) )
-		fromStation = toStation = viaStation = None
-		
-		args = [x.lower() for x in args]
-		
-		codes = []
-		
-		while len( args ) > 0:
-			search = self.__make_station_search_args( args )
-			( code, match ) = self.__get_station_code( search )
-			if code:
-				codes.append( code )
-				args = args[ len( match.split( ' ' ) ) : ]
-			else:
-				args = args[ 1 : ]
-		
-		if len( codes ) < 2:
-			return [ 'Error: niet genoeg stations opgegeven' ]
-		
-		fromStation = codes[0]
-		toStation = codes[1]
-		if len( codes ) > 2:
-			viaStation = codes[1]
-			toStation = codes[2]
-		
-		if not ( fromStation and toStation ):
-			return [ 'Error: geen geldige stations opgegeven.' ]
-		if fromStation == toStation and not viaStation:
-			return [ 'Error: fromStation en toStation zijn gelijk en er is geen viaStation' ]
-		try:
-			args = {
-				'previousAdvices': 0,
-				'nextAdvices': 2,
-				'fromStation': fromStation,
-				'toStation': toStation
-			}
-			if viaStation:
-				args['viaStation'] = viaStation
-			
-			root = self.__apiquery( 'ns-api-treinplanner', args )
-		except NsApiException as e:
-			return [ str( e ) ]
-		
-		if root.tag == 'ReisMogelijkheden':
-			now = datetime.now(tzlocal())
-			response = []
-			i = 0
-			max_results = 3
-			for rm in root:
-				( vertrektijd, vertrektijd_delta ) = self.__parse_tijd( rm.find( 'ActueleVertrekTijd' ).text, now )
-				reistijd = rm.find( 'ActueleReisTijd' ).text
-				overstappen = rm.find( 'AantalOverstappen' ).text
-				response.append(
-					'#{0}: Reistijd {1}; {2} overstappen; vertrekt om {3} (over {4})'.format( 
-						i, reistijd, overstappen, vertrektijd, vertrektijd_delta
-					)
-				)
-				reisdelen = []
-				curr = None
-				max_part_len = ( [ 0, 0, 0 ], [ 0, 0, 0 ] )
-				for reisdeel in rm.findall( 'ReisDeel' ):
-					stops = [[
-							self.__parse_tijd( x.find( 'Tijd' ).text ),
-							x.find( 'Naam' ).text,
-							'sp ' + x.find( 'Spoor' ).text if x.find( 'Spoor' ) is not None else None
-						] if x is not None else None for x in reisdeel.findall( 'ReisStop' )]
-					a = stops[0]
-					b = stops[-1]
-					
-					for j in range( 0, 3 ):
-						if len( a[j] ) > max_part_len[0][j]:
-							max_part_len[0][j] = len( a[j] )
-						if len( b[j] ) > max_part_len[1][j]:
-							max_part_len[1][j] = len( b[j] )
-					
-					reisdelen.append( ( a, b ) )
-				
-				for ( a, b ) in reisdelen:
-					for j in range( 0, 3 ):
-						a[j] = a[j].ljust( max_part_len[0][j] )
-						b[j] = b[j].ljust( max_part_len[1][j] )
-					fmt = '{0}  {1}  {2}'
-					a = fmt.format( *a )
-					b = fmt.format( *b )
-					response.append(
-						'    | In: {0} | Uit: {1} |'.format( a, b )
-					)
-				i += 1
-				if i >= max_results:
-					break
-			if len( response ) == 0:
-				return [ 'Geen resultaten...' ]
-			return response
-		raise Exception()
-	
-	def __vertrektijden( self, station ):
-		try:
-			root = self.__apiquery( 'ns-api-avt', { 'station': station } )
-		except NsApiException as e:
-			return [ str(e) ]
-		if root.tag == 'ActueleVertrekTijden':
-			now = datetime.now(tzlocal())
-			response = []
-			i = 0
-			for vt in root:
-				if i > 5:
-					break
-				( vertrektijd, vertrektijd_delta ) = self.__parse_tijd( vt.find( 'VertrekTijd' ).text, now )
-				treinsoort = vt.find( 'TreinSoort' ).text
-				eindbestemming = vt.find( 'EindBestemming' ).text
-				spoor = vt.find( 'VertrekSpoor' )
-				spoor_str = spoor.text
-				if spoor.attrib['wijziging'] == 'true':
-					spoor_str += ' [!]'
-				response.append( '#{0} {1} naar {2} op spoor {5}; vertrekt om {3} (over {4})'.format( 
-					i, treinsoort, eindbestemming, vertrektijd, vertrektijd_delta, spoor_str 
-				) )
-				i += 1
-			if len( response ) == 0:
-				return [ 'Geen resultaten...' ]
-			return response
-		raise Exception()
-	
-	def __storingen( self, station ):
-		try:
-			root = self.__apiquery( 'ns-api-storingen', { 'station': station } )
-		except NsApiException as e:
-			return [ str(e) ]
-		
-		if root.tag == 'Storingen':
-			response = []
-			#for storing in root.find( 'Ongepland' ):
-			#	pass
-			for storing_type in root:
-				response.append( '{0}:'.format( storing_type.tag ) )
-				for storing in storing_type:
-					traject = storing.find( 'Traject' ).text
-					response.append( '\t* {0}'.format(
-						traject
-					) )
-				else:
-					response.append( '\t* Geen' )
-			return response
-		raise Exception()
-	
-	def __parse_tijd( self, time_str, now = None ):
-		tijd_datetime = dateutil.parser.parse( time_str )
-		if not now:
-			return tijd_datetime.strftime( '%H:%M' )
-		tijd_delta = tijd_datetime - now
-		minuten = tijd_delta.seconds // 60
-		uren = minuten // 60
-		minuten %= 60
-		if uren > 0:
-			tijd_delta = '{0} uur, {1} minuten'.format( uren, minuten )
-		else:
-			tijd_delta = '{0} minuten'.format( minuten )
-		return ( tijd_datetime.strftime( '%H:%M' ), tijd_delta )
-		
+    def __apiquery( self, api_method, args = None ):
+        conn = http.client.HTTPConnection( 'webservices.ns.nl' )
+        conn.request(
+            'GET',
+            '/{0}{1}'.format( api_method, '?' + urllib.parse.urlencode( args ) if args and len( args ) > 0 else '' ),
+            headers = { 'Authorization': 'Basic ' + base64.b64encode( '{0}:{1}'.format( self.username, self.password ).encode( 'utf-8' ) ).decode( 'utf-8' ) }
+        )
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        root = ET.fromstring( data )
+        if root.tag == 'error':
+            raise NsApiException( 'API error: {0}'.format( root[0].text ) )
+        return root
+    
+    def __station_list( self ):
+        try:
+            root = self.__apiquery( 'ns-api-stations-v2' )
+        except NsApiException as e:
+            return [ str( e ) ]
+        if root.tag == 'Stations':
+            list = {}
+            for station in root:
+                namen = []
+                code = station.find( 'Code' ).text
+                land = station.find( 'Land' ).text
+                for naam in station.find( 'Namen' ):
+                    if not naam.text in namen:
+                        namen.append( naam.text )
+                for naam in station.find( 'Synoniemen' ):
+                    if not naam.text in namen:
+                        namen.append( naam.text )
+                list[ code ] = {
+                    'land': land,
+                    'namen': namen
+                }
+            return list
+        raise Exception
+    
+    def __get_station_code( self, arg ):
+        for code in self.stations:
+            for name in self.stations[ code ]['namen']:
+                if name.lower() in arg:
+                    return ( code, name.lower() )
+        return ( False, False )
+    
+    def __make_station_search_args( self, args ):
+        list = []
+        for i in range( 0, len( args ) + 1 ):
+            list.append( ' '.join( args[ :i ] ) )
+        return list
+    
+    def __plan_route( self, args ):
+        print( '__plan_route( {0} )'.format( args ) )
+        fromStation = toStation = viaStation = None
+        
+        args = [x.lower() for x in args]
+        
+        codes = []
+        
+        while len( args ) > 0:
+            search = self.__make_station_search_args( args )
+            ( code, match ) = self.__get_station_code( search )
+            if code:
+                codes.append( code )
+                args = args[ len( match.split( ' ' ) ) : ]
+            else:
+                args = args[ 1 : ]
+        
+        if len( codes ) < 2:
+            return [ 'Error: niet genoeg stations opgegeven' ]
+        
+        fromStation = codes[0]
+        toStation = codes[1]
+        if len( codes ) > 2:
+            viaStation = codes[1]
+            toStation = codes[2]
+        
+        if not ( fromStation and toStation ):
+            return [ 'Error: geen geldige stations opgegeven.' ]
+        if fromStation == toStation and not viaStation:
+            return [ 'Error: fromStation en toStation zijn gelijk en er is geen viaStation' ]
+        try:
+            args = {
+                'previousAdvices': 0,
+                'nextAdvices': 2,
+                'fromStation': fromStation,
+                'toStation': toStation
+            }
+            if viaStation:
+                args['viaStation'] = viaStation
+            
+            root = self.__apiquery( 'ns-api-treinplanner', args )
+        except NsApiException as e:
+            return [ str( e ) ]
+        
+        if root.tag == 'ReisMogelijkheden':
+            now = datetime.now(tzlocal())
+            response = []
+            i = 0
+            max_results = 3
+            for rm in root:
+                ( vertrektijd, vertrektijd_delta ) = self.__parse_tijd( rm.find( 'ActueleVertrekTijd' ).text, now )
+                reistijd = rm.find( 'ActueleReisTijd' ).text
+                overstappen = rm.find( 'AantalOverstappen' ).text
+                response.append(
+                    '#{0}: Reistijd {1}; {2} overstappen; vertrekt om {3} (over {4})'.format( 
+                        i, reistijd, overstappen, vertrektijd, vertrektijd_delta
+                    )
+                )
+                reisdelen = []
+                curr = None
+                max_part_len = ( [ 0, 0, 0 ], [ 0, 0, 0 ] )
+                for reisdeel in rm.findall( 'ReisDeel' ):
+                    stops = [[
+                            self.__parse_tijd( x.find( 'Tijd' ).text ),
+                            x.find( 'Naam' ).text,
+                            'sp ' + x.find( 'Spoor' ).text if x.find( 'Spoor' ) is not None else None
+                        ] if x is not None else None for x in reisdeel.findall( 'ReisStop' )]
+                    a = stops[0]
+                    b = stops[-1]
+                    
+                    for j in range( 0, 3 ):
+                        if len( a[j] ) > max_part_len[0][j]:
+                            max_part_len[0][j] = len( a[j] )
+                        if len( b[j] ) > max_part_len[1][j]:
+                            max_part_len[1][j] = len( b[j] )
+                    
+                    reisdelen.append( ( a, b ) )
+                
+                for ( a, b ) in reisdelen:
+                    for j in range( 0, 3 ):
+                        a[j] = a[j].ljust( max_part_len[0][j] )
+                        b[j] = b[j].ljust( max_part_len[1][j] )
+                    fmt = '{0}  {1}  {2}'
+                    a = fmt.format( *a )
+                    b = fmt.format( *b )
+                    response.append(
+                        '    | In: {0} | Uit: {1} |'.format( a, b )
+                    )
+                i += 1
+                if i >= max_results:
+                    break
+            if len( response ) == 0:
+                return [ 'Geen resultaten...' ]
+            return response
+        raise Exception()
+    
+    def __vertrektijden( self, station ):
+        try:
+            root = self.__apiquery( 'ns-api-avt', { 'station': station } )
+        except NsApiException as e:
+            return [ str(e) ]
+        if root.tag == 'ActueleVertrekTijden':
+            now = datetime.now(tzlocal())
+            response = []
+            i = 0
+            for vt in root:
+                if i > 5:
+                    break
+                ( vertrektijd, vertrektijd_delta ) = self.__parse_tijd( vt.find( 'VertrekTijd' ).text, now )
+                treinsoort = vt.find( 'TreinSoort' ).text
+                eindbestemming = vt.find( 'EindBestemming' ).text
+                spoor = vt.find( 'VertrekSpoor' )
+                spoor_str = spoor.text
+                if spoor.attrib['wijziging'] == 'true':
+                    spoor_str += ' [!]'
+                response.append( '#{0} {1} naar {2} op spoor {5}; vertrekt om {3} (over {4})'.format( 
+                    i, treinsoort, eindbestemming, vertrektijd, vertrektijd_delta, spoor_str 
+                ) )
+                i += 1
+            if len( response ) == 0:
+                return [ 'Geen resultaten...' ]
+            return response
+        raise Exception()
+    
+    def __storingen( self, station ):
+        try:
+            root = self.__apiquery( 'ns-api-storingen', { 'station': station } )
+        except NsApiException as e:
+            return [ str(e) ]
+        
+        if root.tag == 'Storingen':
+            response = []
+            #for storing in root.find( 'Ongepland' ):
+            #   pass
+            for storing_type in root:
+                response.append( '{0}:'.format( storing_type.tag ) )
+                for storing in storing_type:
+                    traject = storing.find( 'Traject' ).text
+                    response.append( '\t* {0}'.format(
+                        traject
+                    ) )
+                else:
+                    response.append( '\t* Geen' )
+            return response
+        raise Exception()
+    
+    def __parse_tijd( self, time_str, now = None ):
+        tijd_datetime = dateutil.parser.parse( time_str )
+        if not now:
+            return tijd_datetime.strftime( '%H:%M' )
+        tijd_delta = tijd_datetime - now
+        minuten = tijd_delta.seconds // 60
+        uren = minuten // 60
+        minuten %= 60
+        if uren > 0:
+            tijd_delta = '{0} uur, {1} minuten'.format( uren, minuten )
+        else:
+            tijd_delta = '{0} minuten'.format( minuten )
+        return ( tijd_datetime.strftime( '%H:%M' ), tijd_delta )
+        
