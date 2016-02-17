82532c07903a5c16b580f274241a3df4392e4ddb
Jasper Seidel <lenny@jawsper.nl>
2016-02-17 19:48:05 +0100

    Fixed google module.

--- modules/google.py
+++ modules/google.py
@@ -16,27 +16,36 @@ class google(Module):
 		except:
 			raise Exception
 
+		self.load_cache()
+
+	def load_cache(self):
 		if not os.path.exists( self.google_cache_file ):
 			with open( self.google_cache_file, 'w' ):
 				pass
 		with open( self.google_cache_file, 'rb' ) as f:
 			try:
-				self.google_cache = pickle.load( f )
+				self.google_cache = pickle.load(f)
+				if not isinstance(self.google_cache, dict):
+					self.google_cache = {}
 			except EOFError:
 				self.google_cache = {}
 
-	def admin_cmd_google_clear_cache( self, args, source, target, admin ):
+	def save_cache(self):
+		with open(self.google_cache_file, 'wb') as f:
+			pickle.dump(self.google_cache, f)
+
+	def admin_cmd_google_clear_cache(self, admin, **kwargs):
 		"""!google_cache_clear: Clear the google cache"""
 		if not admin:
 			return
 		self.google_cache = {}
 	
-	def cmd_google( self, args, source, target, admin ):
+	def cmd_google(self, raw_args, **kwargs):
 		"""!google <query>: Search on google"""
 		if not self.api_key and self.cx:
 			return
 			
-		query = ' '.join( args )
+		query = raw_args
 
 		if not query in self.google_cache:
 			conn = http.client.HTTPSConnection( 'www.googleapis.com' )
@@ -51,8 +60,7 @@ class google(Module):
 			response = conn.getresponse().read().decode('utf-8')
 
 			self.google_cache[ query ] = json.loads( response )
-			with open( self.google_cache_file, 'wb' ) as f:
-				pickle.dump( self.google_cache, f )
+			self.save_cache()
 
 		results = self.google_cache[ query ]
 		if 'items' in results and results['items'] and results['items'][0]:
@@ -64,7 +72,7 @@ class google(Module):
 					results['items'][0]['link']
 				),
 				re.sub( ' {2,}', ' ', snippet),
-				'Meer resultaten: http://www.google.nl/search?q={0}'.format( '+'.join( args ) )
+				'Meer resultaten: http://www.google.nl/search?q={0}'.format(query.replace(' ', '+'))
 			]
 		else:
 			return [ "I'm afraid I can't find that." ]
