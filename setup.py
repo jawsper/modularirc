6847e4dc4ad6722051fd7c2a56310ad68bf237cb
Jasper Seidel <lenny@jawsper.nl>
2017-05-26 18:50:38 +0200

    Move module requirements.

--- setup.py
+++ setup.py
@@ -13,18 +13,23 @@ setup(
     author_email='code@jawsper.nl',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/jawsper/ircbot',
-    license='LICENSE.TXT',
+    url='https://github.com/jawsper/modularirc',
+    license='LICENSE.txt',
     description='A modular, extensible IRC bot.',
     long_description=open('README.md').read(),
     install_requires=[
         'irc',
-        'requests',
-        'python-mpd',
-        'python-dateutil',
-        'hurry.filesize',
-        'pytz',
     ],
+    extras_require={
+        'basemodules':
+        [
+            'requests',
+            'python-mpd',
+            'python-dateutil',
+            'hurry.filesize',
+            'pytz',
+        ]
+    },
 
     entry_points={
         'console_scripts': [
